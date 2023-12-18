3983769c2a19f743cc760f84ba979c3cb098bf6c
Abdillah-MD <ambdi6@gmail.com>
2023-12-18 14:58:38 +0100

    first commit

---
+++ assets/js/cursor/cursor.js
@@ -0,0 +1,7 @@
+// // Permet de laisser une trainer du cursor section : Developer
+// document.addEventListener('mousemove', (e) => {
+//     const customCursor = document.querySelector(".developer");
+//     customCursor.style.left = e.pageX - 15 + 'px';
+//     customCursor.style.top = e.pageY - 15 + 'px';
+//   });
+
